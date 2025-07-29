533ead62a94f2096a031245be028d405babc4182
AndersonkrnAlves <spfc-alves@hotmail.com>
2025-07-28 23:05:19 -0300

    templete String ou templete literals

--- arquivo.js
+++ arquivo.js
@@ -1,1 +1,6 @@
-console.log("digito o que eu quiser");
+const myAge = 36
+const myCar = 'Audi'
+const myString = `minha idade é ${myAge}, estou ficando velho 
+pelo menos estou andando de ${myCar}`//templete literals / Templete string
+
+console.log(myString)
