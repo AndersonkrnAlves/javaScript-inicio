82fe98f84a3223877a2839a7560d189d79b3acbf
AndersonkrnAlves <spfc-alves@hotmail.com>
2025-07-29 00:02:30 -0300

    meu primeiro objeto

---
+++ 2.tiposDeDados.js
@@ -0,0 +1,28 @@
+/*
+2. Numbers -> Números
+*/
+
+/*const Number1 = 32 / 2
+
+console.log(Number1)*/
+
+//objetos
+
+const Anderson = {
+
+    name: 'Anderson',
+    age: '36',
+    address: {
+        
+        street: 'Dos Bobos',
+        number: '25',
+        city: 'Águas lindas de Goiás',
+        state: 'GO',
+        country: 'Brasil'
+
+    }
+}
+
+Anderson.name = 'Stela'
+Anderson.address.city = 'Rio de Janeiro'
+console.log(Anderson)
